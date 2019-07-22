ef0c4dc38a2b9c008e3cebc730a8e41109a80abc
Sidhika Tripathee <sidhika.t@outlook.com>
2019-07-22 15:20:26 -0700

    added tableviewCell, tableviewcontroller, label for each cell

---
+++ eco-/eco-/TaskListViewController.swift
@@ -0,0 +1,36 @@
+//hi 
+//  ViewController.swift
+//  eco-
+//
+//  Created by Sidhika Tripathee on 7/19/19.
+//  Copyright © 2019 Sidhika Tripathee. All rights reserved.
+//
+
+import UIKit
+
+
+
+class TaskListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tasks = [
+        Task(taskName: "sample", taskStatus: false),
+        Task(taskName: "second task", taskStatus: true)
+        ]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as! TaskTableViewCell
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
