1ffa3ae67507b086562650c3ce7ce9801cc86954
AkolyVongdala <akolyv@uw.edu>
2019-07-22 14:53:15 -0700

    Added a struct for OrgCategory

---
+++ eco-/eco-/DonationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DonationViewController.swift
+//  eco-
+//
+//  Created by Akoly Vongdala on 7/22/19.
+//  Copyright © 2019 Sidhika Tripathee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct OrgCategory {
+    
+    var id : Int
+    var category : String
+    var description : String
+    var image : String
+    
+}
+
+class DonationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
