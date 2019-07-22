1edb01178e2a715cb475ec2a1c57aa957401ca63
Sidhika Tripathee <sidhika.t@outlook.com>
2019-07-22 16:36:31 -0700

    fixed rendering problem (thru deleting constraints)

--- eco-/eco-/TaskTableViewCell.swift
+++ eco-/eco-/TaskTableViewCell.swift
@@ -11,15 +11,15 @@ import UIKit
 class TaskTableViewCell: UITableViewCell {
     @IBOutlet weak var taskNameLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
 
 }
