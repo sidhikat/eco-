0a86760ff722a7713bc5404bf033f2c1fd52ed54
AkolyVongdala <akolyv@uw.edu>
2019-07-22 16:23:11 -0700

    Added assets + finished layout without constraint

--- eco-/eco-/DonationViewController.swift
+++ eco-/eco-/DonationViewController.swift
@@ -9,21 +9,36 @@
 import Foundation
 import UIKit
 
-struct OrgCategory {
-    
-    var id : Int
-    var category : String
-    var description : String
-    var image : String
-    
-}
-
 class DonationViewController: UIViewController {
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var subheaderLabel: UILabel!
+    @IBOutlet weak var LogoImage: UIImageView!
+    
+    @IBOutlet weak var BeeButton: UIButton!
+    @IBOutlet weak var TreeButton: UIButton!
+    @IBOutlet weak var SeaButton: UIButton!
+    
+    // Organization Category
+    let orgCategory = [
+        OrgCategory(id: 0, category: "Bees", description: "Save the BEES!", image: "bee"),
+        OrgCategory(id: 1, category: "Trees", description: "Plant more TREES!", image:"tree"),
+        OrgCategory(id: 2, category: "Seas", description: "Clean the SEAS!", image: "sea")
+    ]
+    
+    // Declaring header and subheader description
+    let header: String = "Donation"
+    let subHeader: String = "Choose which organization will benefit from your productivity! For every hour you spent completing your task, we will donate $1 to your chosen organization!"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Displaying the header, subheadr, and image
+        headerLabel.text = header
+        subheaderLabel.text = subHeader
     }
+    
+    
     
     
 }
---
+++ eco-/eco-/Models/OrgCategory.swift
@@ -0,0 +1,19 @@
+//
+//  OrgCategory.swift
+//  eco-
+//
+//  Created by Akoly Vongdala on 7/22/19.
+//  Copyright © 2019 Sidhika Tripathee. All rights reserved.
+//
+
+import Foundation
+
+// model for the organization category (e.g. Bees, Trees, and Seas)
+struct OrgCategory {
+    
+    var id : Int
+    var category : String
+    var description : String
+    var image : String
+    
+}
