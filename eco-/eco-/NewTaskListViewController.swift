dbc68599056aca4be714f1199c33230ac3d994d6
Sidhika Tripathee <sidhika.t@outlook.com>
2019-07-23 14:44:45 -0700

    created new view controllers and new file for VC

---
+++ eco-/eco-/NewTaskListViewController.swift
@@ -0,0 +1,38 @@
+//hi
+//  ViewController.swift
+//  eco-
+//
+//  Created by Sidhika Tripathee on 7/19/19.
+//  Copyright © 2019 Sidhika Tripathee. All rights reserved.
+//
+
+import UIKit
+
+
+class NewTaskListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var tasks = [
+        Task(taskName: "sample", taskStatus: false),
+        Task(taskName: "second task", taskStatus: true)
+    ]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as! TaskTableViewCell
+        cell.taskNameLabel.text = tasks[indexPath.row].taskName
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+}
+
