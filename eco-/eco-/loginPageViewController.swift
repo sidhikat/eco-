d3c6746662728a9a45a6a447d9231e316600e396
Guilherme Hohenfeld <guilhermehohenfeld@minerva.kgi.edu>
2019-07-25 10:09:49 -0700

    Added coreData storage

--- eco-/eco-/loginPageViewController.swift
+++ eco-/eco-/loginPageViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import CoreData
+
 
 class loginPageViewController: UIViewController {
+    var container: NSPersistentContainer!
     
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
@@ -21,10 +24,22 @@ class loginPageViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard container != nil else {
+            fatalError("This view needs a persistent container.")
+            // Do any additional setup after loading the view.
+        }
         // Do any additional setup after loading the view.
 
 //Add action to the buttons on the screen
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // get a reference to the SECOND view controller
+        if  let secondViewController = segue.destination as? signUpScreen {
+            // set a variable in the SECOND view controller with the String to pass
+            secondViewController.container = self.container
+        }
+    }
+    
     @IBAction func loginButton(_ sender: Any) {
 //Requires the user to fill in the username and password text fields
 // Activity indicator and segue to homepage
@@ -32,7 +47,6 @@ class loginPageViewController: UIViewController {
         activityIndicator.hidesWhenStopped = true
         activityIndicator.style = UIActivityIndicatorView.Style.gray
         view.addSubview(activityIndicator)
-        
         activityIndicator.startAnimating()
         
         
@@ -40,22 +54,14 @@ class loginPageViewController: UIViewController {
     }
     
     @IBAction func googleLoginButton(_ sender: Any) {
-        activityIndicator.center = self.view.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.style = UIActivityIndicatorView.Style.gray
-        view.addSubview(activityIndicator)
-        activityIndicator.startAnimating()
+        
         
         
 
     }
 //Activity indicator and segue to homepage
     @IBAction func facebookLoginButton(_ sender: Any) {
-        activityIndicator.center = self.view.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.style = UIActivityIndicatorView.Style.gray
-        view.addSubview(activityIndicator)
-        activityIndicator.startAnimating()
+        
 //Activity indicator and segue to homepage
     }
 }
